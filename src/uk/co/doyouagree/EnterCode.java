401b5f608fa06ec45ca8f62a0ecffc54b0e966de
Tom Berman <tjcberman@gmail.com>
2013-08-29 12:40:01 +0100

    first

---
+++ src/uk/co/doyouagree/EnterCode.java
@@ -0,0 +1,79 @@
+package uk.co.doyouagree;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.*;
+
+public class EnterCode extends Activity {
+	public final static String ENTEREDCODE = "com.example.EnterCode.enteredCode";
+	public final static String USERID = "com.example.EnterCode.userID";
+	private String usrID = "";
+	private EditText code;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_code);
+        
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+          // callback when session changes state
+          @Override
+          public void call(Session session, SessionState state, Exception exception) {
+        	  if (session.isOpened()) {
+        		// make request to the /me API
+        		  Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+        		    // callback after Graph API response with user object
+        		    @Override
+        		    public void onCompleted(GraphUser user, Response response) {
+        		    	if (user != null) {
+        		    		TextView welcome = (TextView) findViewById(R.id.textView2);
+        		    		welcome.setText("Hello " + user.getName() + "!");
+        		    		usrID = user.getId();
+        		    	}
+        		    }
+        		  });
+        		  
+        	  }
+
+          }
+        });
+        
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_enter_code, menu);
+        return true;
+    }
+    
+    public void enterCode(View view) {
+       // label
+    	
+    	code = (EditText) findViewById(R.id.editText1);
+    	
+    	Intent myIntent = new Intent(this, PollResultsView.class);
+    	myIntent.putExtra(ENTEREDCODE, code.getText().toString());
+    	myIntent.putExtra(USERID, usrID);
+    	startActivity(myIntent);
+
+    	
+    }
+    
+}
---
+++ src/uk/co/doyouagree/PollResultsView.java
@@ -0,0 +1,247 @@
+package uk.co.doyouagree;
+
+
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.FacebookRequestError;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+
+@SuppressLint("SetJavaScriptEnabled")
+public class PollResultsView extends Activity {
+	String code = "";
+	String userID = "";
+	private WebView wv;
+	private SeekBar sb;
+	private TextView percent;
+	private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+	private static final String PENDING_PUBLISH_KEY = "pendingPublishReauthorization";
+	private boolean pendingPublishReauthorization = false;
+	private ImageButton shareButton;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_poll_results_view);
+		// Show the Up button in the action bar.
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		wv = (WebView) findViewById(R.id.webView1);
+		wv.getSettings().setJavaScriptEnabled(true);
+		
+		
+		
+		
+		Intent intent = getIntent();
+		code = intent.getStringExtra(EnterCode.ENTEREDCODE);
+		if(code.equals("")) code ="NotACode";
+		//System.out.println(code);
+		
+		userID = intent.getStringExtra(EnterCode.USERID);
+		
+		
+		
+		//String url = "http://pic-card.me/pm/"+code+".html";
+		String url = "http://www.doyouagree.co.uk/"+code+".html";
+			
+		wv.loadUrl(url);
+			
+		shareButton = (ImageButton) findViewById(R.id.shareButton);
+		percent = (TextView) findViewById(R.id.textView3);
+		
+		sb = (SeekBar) findViewById(R.id.seekBar1);
+		sb.setMax(100);
+		sb.setProgress(50);
+		
+		sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+		    @Override
+		    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		        
+		        percent.setText("I Agree : "+String.valueOf(progress)+"%");
+		    }
+
+		    @Override
+		    public void onStartTrackingTouch(SeekBar seekBar) {
+
+		    }
+
+		    @Override
+		    public void onStopTrackingTouch(SeekBar seekBar) {
+
+		    }
+		});
+		
+		if (savedInstanceState != null) {
+		    pendingPublishReauthorization = 
+		        savedInstanceState.getBoolean(PENDING_PUBLISH_KEY, false);
+		}
+
+		
+	}
+	
+	public void goBack(View view) {
+		 
+		shareButton.setVisibility(View.INVISIBLE);
+		Intent returnIntent = new Intent(this, EnterCode.class);
+    	startActivity(returnIntent);
+		 
+	 }
+	
+	public void share(View view) {
+		publishStory();
+		 
+	 }
+	
+	public void sendResult(View view) {
+		int val = sb.getProgress();
+		
+		float sendVal = (float)val / 100;
+		
+		String resultUrl = "http://www.doyouagree.co.uk/storeResponse.php?code="+code+"&value="+sendVal+"&user="+userID;
+			
+	
+		wv.loadUrl(resultUrl);
+		shareButton.setVisibility(View.VISIBLE);
+		
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_poll_results_view, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+//			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void publishStory() {
+	    Session session = Session.getActiveSession();
+
+	    if (session != null){
+
+	        // Check for publish permissions    
+	        List<String> permissions = session.getPermissions();
+	        if (!isSubsetOf(PERMISSIONS, permissions)) {
+	            pendingPublishReauthorization = true;
+	            Session.NewPermissionsRequest newPermissionsRequest = new Session
+	                    .NewPermissionsRequest(this, PERMISSIONS);
+	        session.requestNewPublishPermissions(newPermissionsRequest);
+	            return;
+	        }
+
+	        String lnk = "http://www.doyouagree.co.uk/storeResponse.php?code="+code;
+	        Bundle postParams = new Bundle();
+	        postParams.putString("name", "Do You Agree");
+	        postParams.putString("caption", "Do you agree with ...");
+	        postParams.putString("description", "The mobile app that makes it easy to see if you agree");
+	        postParams.putString("link", lnk);
+	        postParams.putString("picture", "http://www.doyouagree.co.uk/dya_logo.png");
+
+	        Request.Callback callback= new Request.Callback() {
+	            public void onCompleted(Response response) {
+	                JSONObject graphResponse = response
+	                                           .getGraphObject()
+	                                           .getInnerJSONObject();
+	                String postId = null;
+	                try {
+	                    postId = graphResponse.getString("id");
+	                } catch (JSONException e) {
+	                    Log.i("Fb JSON Exception",
+	                        "JSON error "+ e.getMessage());
+	                    
+	                }
+	                FacebookRequestError error = response.getError();
+	                if (error != null) {
+	                    Toast.makeText(getApplicationContext(),
+	                         error.getErrorMessage(),
+	                         Toast.LENGTH_SHORT).show();
+	                    } else {
+	                        Toast.makeText(getApplicationContext(), 
+	                             postId,
+	                             Toast.LENGTH_LONG).show();
+	                }
+	            }
+	        };
+
+	        Request request = new Request(session, "me/feed", postParams, 
+	                              HttpMethod.POST, callback);
+
+	        RequestAsyncTask task = new RequestAsyncTask(request);
+	        task.execute();
+	    }
+
+	}
+	private boolean isSubsetOf(Collection<String> subset, Collection<String> superset) {
+	    for (String string : subset) {
+	        if (!superset.contains(string)) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	@SuppressWarnings("unused")
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    if (state.isOpened()) {
+	        shareButton.setVisibility(View.VISIBLE);
+	        if (pendingPublishReauthorization && 
+	                state.equals(SessionState.OPENED_TOKEN_UPDATED)) {
+	            pendingPublishReauthorization = false;
+	            publishStory();
+	        }
+	    } else if (state.isClosed()) {
+	        shareButton.setVisibility(View.INVISIBLE);
+	    }
+	}
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    outState.putBoolean(PENDING_PUBLISH_KEY, pendingPublishReauthorization);
+//	    super.uiHelper.onSaveInstanceState(outState);
+	}
+	
+	
+}
